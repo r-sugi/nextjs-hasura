622cb5c55df333bc0ca47d6237fa4fe2c8a9eb56
r-sugi <r.sugimoto0615@gmail.com>
2021-09-01 10:47:37 +0900

    15. Redux toolkit in Nextjs

---
+++ types/types.ts
@@ -0,0 +1,18 @@
+export interface News {
+  id: string
+  content: string
+  created_at: string
+}
+export interface EditNews {
+  id: string
+  content: string
+}
+export interface Task {
+  id: string
+  title: string
+  created_at: string
+}
+export interface EditTask {
+  id: string
+  title: string
+}
